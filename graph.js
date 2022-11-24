c83ee9c4d223d303f901976f77d787fbaea91e9e
Connor Wales <connorwales@gmail.com>
2022-11-24 13:01:08 -0800

    completed, all required tests passing

--- graph.js
+++ graph.js
@@ -11,25 +11,81 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let neighbor of vertex.adjacent) {
+      this.removeEdge(vertex, neighbor)
+    }
+    
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const toSearch = [start];
+    const searched = new Set();
+    const outArr = [];
+
+    while (toSearch.length > 0) {
+      let currentNode = toSearch.pop();
+      outArr.push(currentNode.value);
+
+      for (let neighbor of currentNode.adjacent) {
+        if (!searched.has(neighbor)) {
+          toSearch.push(neighbor);
+          searched.add(neighbor);
+        }
+      }
+      searched.add(currentNode);
+    }
+
+    return outArr;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const toSearch = [start];
+    const searched = new Set();
+    const outArr = [];
+
+    while (toSearch.length > 0) {
+      let currentNode = toSearch.pop();
+      outArr.push(currentNode.value);
+
+      for (let neighbor of currentNode.adjacent) {
+        if (!searched.has(neighbor)) {
+          toSearch.unshift(neighbor);
+          searched.add(neighbor);
+        }
+      }
+      searched.add(currentNode);
+    }
+
+    return outArr;
+  }
 }
 
 module.exports = {Graph, Node}
